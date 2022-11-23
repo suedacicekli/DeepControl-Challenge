804cd8028b6c790237ffaba44ded871a3fce9545
suedacicekli <suedacicekli@gmail.com>
2022-11-23 12:50:53 +0300

    🚀 Add | Project created and navbar added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <img
+          src="https://deepcontrol.net/assets/img/deepcontrol_black_logo.png"
+          width="15%"
+          className="d-inline-block align-top"
+          alt="React Bootstrap logo"
+        />
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Kurumsal</Nav.Link>
+            <Nav.Link href="#action2">IOT Çözümleri</Nav.Link>
+
+            <Nav.Link href="#" disabled>
+              Deep Control Lab
+            </Nav.Link>
+          </Nav>
+          <div >
+            <Button variant="outline-primary" >Login</Button>
+            <Button variant="primary" className=" mx-3 ">Sign Up</Button>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
